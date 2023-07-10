0085016ac867368c64b4dbdcebaa2421b160efc4
JuniorPacheco <junior.pacheco.356@gmail.com>
2023-07-10 11:08:30 -0500

    fixed problem connection database

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize(
-  process.env.POSTGRESQL_DATABASE,
-  process.env.POSTGRESQL_USER,
-  process.env.POSTGRESQL_PASSWORD,
-  {
-    host: process.env.POSTGRESQL_HOST,
-    dialect: process.env.POSTGRESQL_DIALECT,
-  }
-);
+const db = new Sequelize(process.env.POSTGRESQL_DATABASE_URL);
 
 module.exports = db;
