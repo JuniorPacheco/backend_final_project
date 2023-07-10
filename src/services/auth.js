0a49334276dad5112eeeaaad7f802ace1d2615f8
JuniorPacheco <junior.pacheco.356@gmail.com>
2023-07-10 10:42:49 -0500

    try catch on route login

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,26 +2,29 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const { loginUser } = require("../controllers/auth");
 
-
 const login = async (req, res) => {
-  const data = req.body;
+  try {
+    const data = req.body;
 
-  const response = await loginUser(data.email, data.password);
+    const response = await loginUser(data.email, data.password);
 
-  if (response) {
-    const token = jwt.sign(
-      {
-        id: response.id,
-        email: response.email,
-        name: response.name,
-      },
-      process.env.JWT_SECRET
-    );
-    return res
-      .status(200)
-      .json({ message: "All ok, valid credentials", token });
-  } else {
-    return res.status(401).json({ message: "Invalid Credentials" });
+    if (response) {
+      const token = jwt.sign(
+        {
+          id: response.id,
+          email: response.email,
+          name: response.name,
+        },
+        process.env.JWT_SECRET
+      );
+      return res
+        .status(200)
+        .json({ message: "All ok, valid credentials", token });
+    } else {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err });
   }
 };
 
