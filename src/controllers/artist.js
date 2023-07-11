e55eb762ac0a8694d97c6c5d1b6d222d440d5c5a
JuniorPacheco <junior.pacheco.356@gmail.com>
2023-07-11 15:32:10 -0500

    improved artist endpoint by id adding top songs and artist albums

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -4,12 +4,25 @@ const { getConfig } = require("../utils/token");
 const getArtistById = async (artistId) => {
   const config = await getConfig();
 
-  const { data } = await axios.get(
+  const { data: dataArtist } = await axios.get(
     `https://api.spotify.com/v1/artists/${artistId}`,
     config
   );
 
-  return data;
+  const { data: songsTop } = await axios.get(
+    `https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=ES`,
+    config
+  );
+
+  const { data: albums } = await axios.get(
+    `https://api.spotify.com/v1/artists/${artistId}/albums`,
+    config
+  );
+
+  dataArtist.songsTop = songsTop.tracks
+  dataArtist.albums = albums.items
+
+  return dataArtist;
 };
 
 module.exports = { getArtistById };
